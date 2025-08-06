4016e6cbc79a18d9a632ca0900386ba364b49225
Ruchit2993 <ruchitpitaliya2003@gmail.com>
2025-08-06 18:22:28 +0530

    Updated Tasks till 06-Aug

--- JavaScript/Introduction/new.js
+++ JavaScript/Introduction/new.js
@@ -1,5 +1,35 @@
-let abc = " 1 2 3 "
-let def = parseInt(abc);
-console.log(def);
+// let abc = " 1 2 3 "
+// let def = parseInt(abc);
+// console.log(def);
 
-console.log(true == "true");
+// console.log(true == "true");
+
+	
+// let a =0;
+// function x(){
+//     // let a = 3;
+//     // console.log(typeof(a))
+//     // console.log(a);
+//     let y = () => {
+//         console.log(a)
+//       }
+//       return y;
+// }
+
+// let z=x();
+// console.log(z)
+// z();
+
+// let wm = new WeakMap();
+// // let user = {name:"dev",lname:"it"};
+// wm.set(user,23)
+// console.log(wm);
+// console.log(wm.get(user));
+// console.log(user);
+
+// let ws = new WeakSet();
+// let user = [1,2,3,4,8,5,2,5,4,1,2,1]
+// console.log(user)
+// user = new WeakSet();
+// console.log(user)
+
